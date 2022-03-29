d9158f9ad65f00c762124ec69aa7aeb44060e1b8
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-03-29 22:43:34 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ Write a program to print Sum, Product and Quotient of two numbers 22 and 7.
+ */
+
+int main(){
+
+int a,b;
+
+scanf("%d",&a);
+scanf("%d",&b);
+printf("Sum of the 2 numbers is: %d\n",a+b);
+printf("Difference of the 2 numbers is: %d\n",a-b);
+printf("Product of the 2 numbers is: %d\n",a*b);
+printf("Quotient of the 2 numbers is: %d\n",a/b);
+return 0;
+}

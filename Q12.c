e7f36d53992dec97f86d6610ee213fc2fac32d7d
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-04-14 18:24:32 +0530

    Add files via upload

---
+++ Q12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int num=2;
+switch(num+2){
+case 1:
+printf("Case 1: ");
+case 2:
+printf("Case 2: ");
+case 3:
+printf("Case 3: ");
+default:
+printf("Default: ");
+}
+return 0;
+}

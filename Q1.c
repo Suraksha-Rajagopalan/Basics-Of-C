ee31ced19cc851f76319892455a54362d7a3a81e
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-04-14 17:20:08 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+int a=10, b=4;
+
+if (a>b)
+printf("a is greater than b\n");
+else
+printf("a is less than or equal to b\n");
+if (a!=b)
+printf("a is not equal to b\n");
+else
+printf("a is equal to b\n");
+return 0;
+}

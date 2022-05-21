399d16c8a4e4684e4fd5b5f8029793cd8c6b348a
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-05-21 17:33:49 +0530

    Create PredictOutput_4.c

---
+++ PredictOutput_4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//Here the for loop gets executed 5 times, which increments the value of i till 6
+int main(){
+    int i;   
+    for(i=0;i<=5;i++);  printf("%d",i);  
+    return 0; 
+    
+}

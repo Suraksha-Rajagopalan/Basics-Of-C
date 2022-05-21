f97750c6293e7012a78a93e513bdeb0b1347a60a
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-05-21 17:43:49 +0530

    Create PredictOutput_6.c

---
+++ PredictOutput_6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Here the variables i,j,k are given the initial values with the condition as i<=4 and i's value is incremented
+//This gives us the output as 3 4 5 6 7
+int main(){
+   int i,j,k;           
+   for(i=0,j=2,k=1;i<=4;i++){  printf("%d ",i+j+k);   } 
+
+    return 0; 
+    
+}

373fda443cdde704d1389e97a5334db8e33be005
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-05-21 11:59:15 +0530

    Create PredictOutput_2.c

---
+++ PredictOutput_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// Here until the value of loop doesn't become zero the while loop's condition is true and hence gets executed.
+int main(){
+    int loop=10;
+    while(printf("Hello ") && loop--); 
+    return 0; 
+    
+}

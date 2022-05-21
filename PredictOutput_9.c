f8e65d402619ffe5ab2277dd0eec82045360892c
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-05-21 17:58:26 +0530

    Create PredictOutput_9.c

---
+++ PredictOutput_9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Here the loop runs infinite amount of times
+//Except for the case where n is not equal to zero for the rest of the case the loop runs. 
+int main(){
+   int n;    
+   for (n = 9; n!=0; n--)   
+   printf("n = %d ", n--);
+
+
+    return 0; 
+    
+}

d1251b5c188860cde786cadd01384bed4dbde1ad
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-05-21 17:45:37 +0530

    Create PredictOutput_7.c

---
+++ PredictOutput_7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Here Hello gets printed thrice
+int main(){
+   double k = 0;          
+   for (k = 0.0; k < 3.0; k++)             
+   printf("Hello");   
+
+    return 0; 
+    
+}

5de4343d21e22f51d5859b1ff8a7ae4d7c67dafc
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-05-21 17:38:00 +0530

    Create PredictOutput_5.c

---
+++ PredictOutput_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//Here the while loop executes 5 times while the do while loop gets executed only once since the condition of the do-while loop is false.
+int main(){
+   int i;            
+   for(i=10;i<=15;i++){           
+       while(i){                 
+           do{                     
+               printf("%d ",1);                   
+               if(i>>1)                                
+               continue;                   
+               }while(0);   
+               break;   }          }   
+    return 0; 
+    
+}

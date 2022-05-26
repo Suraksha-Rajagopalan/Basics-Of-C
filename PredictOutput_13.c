a39f78ede13eb21807ca83f18643b536c55d4fa0
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-05-26 21:07:41 +0530

    Create PredictOutput_13.c

---
+++ PredictOutput_13.c
@@ -0,0 +1,4 @@
+#include <stdio.h> 
+//The output of the following code is 1; here the value of cnt is incremented and is printed.
+void main()  {  
+char cnt=0; for(;cnt++;printf("%d",cnt)) ; printf("%d",cnt);  } 
